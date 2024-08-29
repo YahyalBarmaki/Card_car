fdcdc0cbb353310ab3280a734dc0c1324fa4667d
Yaya LY <yaya.ly@uadb.edu.sn>
2024-08-29 09:41:29 +0000

    added car model and color and pager

--- app/src/main/java/com/example/cardcar/MainActivity.kt
+++ app/src/main/java/com/example/cardcar/MainActivity.kt
@@ -1,47 +1,74 @@
 package com.example.cardcar
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.material3.rememberTopAppBarState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.cardcar.ui.theme.CardCarTheme
+import dev.chrisbanes.haze.HazeState
+import dev.chrisbanes.haze.haze
+import dev.chrisbanes.haze.hazeChild
 
 class MainActivity : ComponentActivity() {
+    @OptIn(ExperimentalMaterial3Api::class)
+    @SuppressLint("RememberReturnType")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             CardCarTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                val hazeState = remember { HazeState() }
+                val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior(
+                    state = rememberTopAppBarState()
+                )
+                Scaffold(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .nestedScroll(scrollBehavior.nestedScrollConnection)
+                        .background(MaterialTheme.colorScheme.background),
+                    containerColor = Color.Transparent,
+                    topBar = {
+                        Column {
+                            TopBar(
+                                modifier = Modifier.hazeChild(state = hazeState),
+                                scrollBehavior = scrollBehavior
+                            )
+                            Pager(
+                                modifier = Modifier
+                                    .fillMaxSize()
+                                    .hazeChild(state = hazeState)
+                            )
+                        }
+
+                    },
+
+                    ) { innerPadding ->
+                    HomeScreen(modifier = Modifier.padding(innerPadding))
                 }
             }
         }
     }
-}
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+    @Composable
+    fun HomeScreen(modifier: Modifier = Modifier) {
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    CardCarTheme {
-        Greeting("Android")
     }
 }
+
